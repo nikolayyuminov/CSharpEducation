19a9053a1fac6db53f98cbaa50d4b49b82e716eb
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-11 19:57:24 +0400

    Практика2 задача2

--- CSharpEducation.Practice/Practice2.Task1/obj/Debug/net8.0/Practice2.Task1.AssemblyInfo.cs
+++ CSharpEducation.Practice/Practice2.Task1/obj/Debug/net8.0/Practice2.Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice2.Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ae4017431b530afa4e173d1d2d14631a7d1d22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33ac393ee0a964b9d03292de21cb9282d003bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice2.Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice2.Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
