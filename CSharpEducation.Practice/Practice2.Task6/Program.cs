e81d369f0e72dd4dbdeed0aaa900e0d659f8b3d2
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-12 14:41:29 +0400

    Практика 2 задача 6

---
+++ CSharpEducation.Practice/Practice2.Task6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice2.Task6;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 6!");
+    if (args.Length == 0)
+    {
+      Console.Write("Введите строку: ");
+      string input = Console.ReadLine();
+      Console.Write("Введите искомый символ: ");
+      char inputSymbol = char.Parse(Console.ReadLine());
+      Console.WriteLine(Percentage(input, inputSymbol));
+    }
+    else
+    {
+      Console.WriteLine(Percentage(args[0], char.Parse(args[1])));
+    }
+  }
+
+  static double Percentage(string args, char symbol)
+  {
+    int counter = 0;
+    if (string.IsNullOrWhiteSpace(args))
+    {
+      args = "TestString";
+      symbol = 't';
+      counter = args.Count(c => c == symbol);
+
+      return (double)counter * 100 / args.Length;
+
+    }
+    counter = args.Count(c => c == symbol);
+
+    return (double)counter * 100 / args.Length;
+  }
+}
