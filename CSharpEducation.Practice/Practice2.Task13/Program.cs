396b697619f0144f50dfaef6ec4ed7b5aeaf3a9b
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-12 17:11:33 +0400

    Практика2 задача 13

---
+++ CSharpEducation.Practice/Practice2.Task13/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Practice2.Task13;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 13!");
+    var matrix = CreateMatrix(5, 5);
+    PrintMassive(matrix);
+
+  }
+  
+  static int [,] CreateMatrix(int rows, int cols)
+  {
+    var random = new Random();
+    var matrix = new int[rows, cols];
+    foreach (var row in Enumerable.Range(0, rows))
+    {
+      foreach (var col in Enumerable.Range(0, cols))
+      {
+        matrix[row, col] = random.Next();
+      }
+    }
+    return matrix;
+  }
+
+  static void PrintMassive(int[,] matrix)
+  {
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+        Console.Write($"{matrix[i, j]} ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
