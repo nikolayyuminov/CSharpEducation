cd3ea21545233cf59fe41d02d43c05aa24729d98
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-12 15:31:46 +0400

    Практика 2 задача 8

---
+++ CSharpEducation.Practice/Practice2.Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Practice2.Task8;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 8!");
+    
+    bool isEqual = true;
+    Console.Write("Введите число 1: ");
+    var a = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    var b = int.Parse(Console.ReadLine());
+    Console.Write("Введите число 3: ");
+    var c = int.Parse(Console.ReadLine());
+
+    if (a != b && c != a && c != b)
+    {
+      Console.WriteLine("Равных нет");
+    }
+    else
+    {
+      a +=5;
+      b +=5;
+      c +=5;
+      Console.WriteLine($"{a}, {b}, {c}");
+    }
+  }
+}
