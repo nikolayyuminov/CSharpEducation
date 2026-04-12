9b74e1985d81410db08f4bea1cb65df8536c580e
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-12 21:49:10 +0400

    Практика 2 задача 20

---
+++ CSharpEducation.Practice/Practice2.Task20/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Practice2.Task20;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    
+    Console.WriteLine("Задача 20!");
+    int[,] mass = Massive(6,6);
+    PrintMassive(mass);
+    Console.WriteLine();
+    Console.WriteLine(DifferenceMaxMin(mass));
+
+  }
+  
+  static int [,] Massive(int row, int col)
+  {
+    var random = new Random();
+    int [,] mass = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+      for (int j = 0; j < col; j++)
+      {
+        mass[i, j] = random.Next(1,101);
+      }
+    }
+    return mass;
+  }
+
+  static int DifferenceMaxMin(int[,] mass)
+  {
+    var max = 0;
+    var min = 100;
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+      for (int j = 0; j < mass.GetLength(1); j++)
+      {
+        if  (mass[i, j] > max)
+        {
+          max = mass[i, j];
+        }
+        else if (mass[i, j] < min)
+        {
+          min = mass[i, j];
+        }
+      }
+    }
+    return max - min;
+  }
+
+  static void PrintMassive(int[,] mass)
+  {
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+      for (int j = 0; j < mass.GetLength(1); j++)
+      {
+        Console.Write(mass[i, j] + " ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
