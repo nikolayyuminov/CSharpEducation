2b7306e3afb715169e930f16bf46b1144c28840b
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-13 18:22:39 +0400

    Практика 2 Задача 24

---
+++ CSharpEducation.Practice/Practice2.Task24/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Practice2.Task24;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 24!");
+    foreach(var day in Enum. GetValues<DayOfWeek>())
+    {
+      Console.WriteLine(day);
+    }
+  }
+
+  enum DayOfWeek
+  {
+    Понедельник,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Воскресенье
+  }
+}
