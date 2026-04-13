2da32415f3d6806476729eda18f7e1484420e22e
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-13 18:28:38 +0400

    Практика 2 задача 25

---
+++ CSharpEducation.Practice/Practice2.Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice2.Task25;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 25!");
+    
+    Console.Write("Введите номер дня недели: ");
+    var result = Convert.ToInt32(Console.ReadLine());
+    
+    Console.WriteLine($"{(DayOfWeek)result}");
+  }
+
+  enum DayOfWeek
+  {
+    Понедельник = 1,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятница,
+    Суббота,
+    Воскресенье
+  }
+}
