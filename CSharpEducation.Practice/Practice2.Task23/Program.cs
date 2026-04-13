ce642e45f61b3762872df829138418886a902615
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-13 17:57:23 +0400

    Практика 2 задача 23

---
+++ CSharpEducation.Practice/Practice2.Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice2.Task23;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Задача 23!");
+    
+    Console.Write("Введите год: ");
+    if (int.TryParse(Console.ReadLine(), out int result))
+    {
+      if (result == 400) Console.WriteLine("Год високосный");
+      else if (result % 100 == 0) Console.WriteLine("Год НЕ високосный");
+      else if (result % 4 == 0) Console.WriteLine("Год високосный");
+      else Console.WriteLine("Год НЕ високосный");
+    }
+    
+    
+  }
+}
