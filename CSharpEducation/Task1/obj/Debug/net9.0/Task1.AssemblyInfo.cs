b2e29d1758e797f1454ae78b71e80500c8a31fa5
Николай Юминов <Yuminov_ns@directum.ru>
2026-04-24 14:21:54 +0400

    удалил obj/debug

--- CSharpEducation/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
+++ CSharpEducation/Task1/obj/Debug/net9.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771913518b5b870a86a97af43ad941afaf23d60d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a50e5f1b3e005846794ce45172360856a68ca0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
