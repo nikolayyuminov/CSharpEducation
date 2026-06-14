52e33cac3a2b19e5cf1c6fc85a7892599e88cbdd
Николай Юминов <Yuminov_ns@directum.ru>
2026-06-14 10:42:46 +0400

    тестовый коммит

---
+++ CSharpEducation.Practice/Practice7.Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Practice4.MathHelper;
+
+namespace Practice7.Task1;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Task1");
+    int a;
+    int b;
+    do
+    {
+      Console.WriteLine("Введите 2 числа через пробел:");
+      string input = Console.ReadLine();
+      var parts = input.Split(' ');
+
+      try
+      {
+        a = int.Parse(parts[0]);
+        b = int.Parse(parts[1]);
+        break;
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e);
+        Console.WriteLine("Попробуйте ввести число");
+        continue;
+      }
+    } while (true);
+
+    Console.WriteLine(MathHelper.Division(a, b));
+
+  }
+  
+  
+}
--- CSharpEducation/Task1/Test1Class.cs
+++ CSharpEducation/Task1/Test1Class.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Globalization;
+
 namespace Task1;
 
-public class Test1Class
+public class TestClass
 {
-  
+  public static void TetsMain()
+  {
+    Console.WriteLine("какая то строка");
+  }  
 }
