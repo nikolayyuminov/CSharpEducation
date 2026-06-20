a44f64dcb155c9b94cb57de5673a94bdb32b86c5
Николай Юминов <Yuminov_ns@directum.ru>
2026-06-20 14:56:25 +0400

    Практика 8 задачи 1,2

---
+++ CSharpEducation.Practice/Practice8.Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Practice8.Task1;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Task1!");
+    
+    Console.Write("Введите список чисел через пробел: ");
+    List<int> numbers = Console.ReadLine()
+      .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+      .Select(int.Parse)
+      .ToList();
+    
+    Console.WriteLine("Выберите порядок сортировки:");
+    Console.WriteLine("1 - По возрастанию");
+    Console.WriteLine("2 - По убыванию");
+
+    string choice = Console.ReadLine();
+
+    Comparison<int> comparison;
+
+    switch (choice)
+    {
+      case "1":
+        comparison = CompareAscending;
+        break;
+      case "2":
+        comparison = CompareDescending;
+        break;
+      default:
+        Console.WriteLine("Неверный выбор!");
+        return;
+    }
+    
+    numbers.Sort(comparison);
+    foreach (var number in numbers)
+    {
+      Console.Write($"{number} ");
+    }
+  }
+  
+  public static int CompareAscending(int a, int b) => a < b ? -1 : 1;
+  
+  public static int CompareDescending(int a, int b) => a > b ? -1 : 1;
+}
---
+++ CSharpEducation.Practice/Practice8.Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Practice8.Task2;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Task2!");
+    
+    Console.Write("Введите слова через пробел: ");
+    List<string> strings = Console.ReadLine()
+      .Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+    
+    Console.WriteLine("Выберите порядок сортировки:");
+    Console.WriteLine("1 - Первый символ 'А'");
+    Console.WriteLine("2 - Короче 5-ти символов");
+
+    string choice = Console.ReadLine();
+
+    Func<string, bool> func;
+
+    switch (choice)
+    {
+      case "1":
+        func = StartWithA;
+        break;
+      case "2":
+        func = Str5Simbols;
+        break;
+      default:
+        Console.WriteLine("Неверный выбор!");
+        return;
+    }
+    
+    var result = strings.Where(func);
+    foreach (var _string in result)
+    {
+      Console.Write($"{_string} ");
+    }
+  }
+  
+  public static bool StartWithA(string str)
+  {
+    
+    return str.ToLower().StartsWith('a');
+  }
+  
+  public static bool Str5Simbols(string str)
+  {
+    return str.Length <= 5;
+  }
+}
